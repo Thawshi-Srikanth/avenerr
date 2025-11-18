816d081e14791a27445c2ef3e0886eb8d269b260
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-11-18 17:53:05 +0530

    feat: nav and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import ContentSection from "@/components/content-1";
 import CTA from "@/components/cta";
 import Features from "@/components/features-3";
 import FeaturesSection from "@/components/features-7";
+import FooterSection from "@/components/footer";
+import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
 import LogoCloud from "@/components/logo-cloud";
 import StatsSection from "@/components/stats-two";
@@ -11,6 +13,7 @@ import TeamSection from "@/components/team";
 export default function Home() {
   return (
     <div>
+      <HeroHeader />
       <HeroSection />
       <LogoCloud />
       <ContentSection />
@@ -20,6 +23,7 @@ export default function Home() {
       <TeamSection />
       <CTA />
       <Contact />
+      <FooterSection />
     </div>
   );
 }
