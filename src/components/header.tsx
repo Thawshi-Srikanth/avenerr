9ce24995854fec55bff83a18641c46cc53fa5541
Thawshi-Srikanth <thawshi.dev@protonmail.com>
2026-01-21 18:02:50 +0530

    refactor: remove `legacyBehavior` and `passHref` from `Link` components.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -203,7 +203,9 @@ export const HeroHeader = () => {
                   </NavigationMenuItem>
                   {mainLinks.map((link) => (
                     <NavigationMenuItem key={link.name}>
-                      <Link href={link.href} legacyBehavior passHref>
+                      <Link href={link.href}>
+                        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                        }
                         <NavigationMenuLink
                           className={cn(
                             navigationMenuTriggerStyle(),
